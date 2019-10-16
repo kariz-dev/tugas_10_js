74e6e5477b6e7dec66aeac39d032dc04a45a5465
Muhammad Rizwan <muhammad.rizwan76146@gmail.com>
2019-10-17 00:48:03 +0700

    tugas 10 javascript Muhammad Rizwan

---
+++ tugas10.js
@@ -0,0 +1,12 @@
+/* tugas 10 javascript */
+// Muhammad Rizwan
+
+function tugasSepuluh(){
+    var m = 0;
+    for(var i=0; i<=10; i++){
+        m=m+i;
+        console.log(m);
+    }
+}
+
+tugasSepuluh();
